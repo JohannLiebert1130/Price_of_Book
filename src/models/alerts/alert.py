96073a6f4d9b2e62a03945561abe0db424668035
JohannLiebert1130 <672016243@qq.com>
2017-11-30 15:19:12 +0800

    Setting up our Alert to sending emails.

--- src/models/alerts/alert.py
+++ src/models/alerts/alert.py
@@ -1,12 +1,33 @@
 import uuid
+import datetime
+import requests
+from src.common.database import Database
+import src.models.alerts.constants as AlertConstants
+from src.models.items.item import Item
+
+__author__ = 'jslvtr'
 
 
 class Alert(object):
-    def __init__(self, user, price_limit, item, _id):
-        self.user = user
+    def __init__(self, user_email, price_limit, item_id, active=True, last_checked=None, _id=None):
+        self.user_email = user_email
         self.price_limit = price_limit
-        self.item = item
+        self.active = active
+        self.item = Item.get_by_id(item_id)
+        self.last_checked = datetime.datetime.utcnow() if last_checked is None else last_checked
         self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
-        return "<Alert for {} on item {} with price {}>".format(self.user, self.item, self.price_limit)
+        return "<Alert for {} on item {} with price {}>".format(self.user_email, self.item.name, self.price_limit)
+
+    def send(self):
+        return requests.post(
+            AlertConstants.URL,
+            auth=("api", AlertConstants.API_KEY),
+            data={
+                "from": AlertConstants.FROM,
+                "to": self.user_email,
+                "subject": "Price limit reached for {}".format(self.item.name),
+                "text": "We've found a deal! ({}).".format(self.item.url)
+            }
+        )

d37ca86be64d75d81654829f8ac7655fbcfd0785
JohannLiebert1130 <672016243@qq.com>
2017-12-01 11:15:28 +0800

    Serving endpoints dynamically using url_for in jinja templates.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 from src.common.database import Database
+from src.models.alerts.views import alert_blueprint
+from src.models.stores.views import store_blueprint
 from src.models.users.views import user_blueprint
 
 app = Flask(__name__)
@@ -20,3 +22,5 @@ def home():
 
 
 app.register_blueprint(user_blueprint, url_prefix="/users")
+app.register_blueprint(alert_blueprint, url_prefix="/alerts")
+app.register_blueprint(store_blueprint, url_prefix="/stores")

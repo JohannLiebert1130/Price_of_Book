d31556765ed1d382aada332bab36f633f0e6ca06
JohannLiebert1130 <672016243@qq.com>
2017-12-03 18:13:10 +0800

    Setting up the environment variables.

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,5 +1,7 @@
-URL = "https://api.mailgun.net/v3/<>.mailgun.org/messages"
-API_KEY = "key-<>"
-FROM = "Mailgun Sandbox <postmaster@<>.mailgun.org>"
+import os
+
+URL = os.environ.get('MAILGUN_URL')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get("MAILGUN_FROM")
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
